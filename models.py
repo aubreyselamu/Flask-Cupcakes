8f1f0aec8487c660ccc9f26d5ac246aad0900a19
Aubrey Selamu-Bell <aubrey.selamu@unknown38f9d3d042db.home>
2021-12-14 13:00:02 -0500

    created Cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,32 @@
+from typing import Sized
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
 """Models for Cupcake app."""
+
+class Cupcake(db.Model):
+    '''Cupcake model'''
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable = False)
+    rating = db.Column(db.Float, nullable = False)
+    image = db.Column(db.Text, default = 'https://tinyurl.com/demo-cupcake')
+    
+    def serialize_cupcake(self):
+        '''Serialize a cupcake SQLAlchemy obj to a dictionary'''
+
+        return {
+            "id" : self.id,
+            "flavor" : self.flavor,
+            "size" : self.size,
+            "rating": self.rating,
+            "image" : self.image
+        }
